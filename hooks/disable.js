ea25451979d794be41099967e40afc0178516f94
Alexandr Salin <AlexandrSalin@users.noreply.github.com>
2016-09-15 13:24:02 +0300

    Update disable.js

--- hooks/disable.js
+++ hooks/disable.js
@@ -1,27 +1,3 @@
 module.exports = function(context) {
   throw new Error('Check AllowBackup plugin start work');
-  var fs = context.requireCordovaModule('fs'),
-    path = context.requireCordovaModule('path');
-
-  var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-
-
-  var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
-
-  if (fs.existsSync(manifestFile)) {
-
-    fs.readFile(manifestFile, 'utf8', function (err,data) {
-      if (err) {
-        throw new Error('Unable to find AndroidManifest.xml: ' + err);
-      }
-      var appClass = ' android:allowBackup="true" ';
-      if (data.indexOf(appClass) == -1) {
-        var result = data.replace(/<application/g, '<application ' + appClass);
-        fs.writeFile(manifestFile, result, 'utf8', function (err) {
-          if (err) throw new Error('Unable to write into AndroidManifest.xml: ' + err);
-        })
-      }
-    });
-  }
-
-};
+}
